176558e984c1a3668f41df1a669a40581f15260a
Yu Xiang <yxiang@singulargenomics.com>
2024-08-21 11:39:06 -0700

    update aero emoji, kitty config and lua c-a option

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+vim.api.nvim_create_augroup("cursorline_cursorcolumn", { clear = true })
+vim.api.nvim_create_autocmd("InsertLeave", {
+  group = "cursorline_cursorcolumn",
+  pattern = "*",
+  command = "set cursorline cursorcolumn",
+})
+vim.api.nvim_create_autocmd("InsertEnter", {
+  group = "cursorline_cursorcolumn",
+  pattern = "*",
+  command = "set nocursorline nocursorcolumn",
+})
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,3 +5,4 @@ local opt = vim.opt
 opt.pumblend = 0
 opt.winbar = "%=%m %f"
 opt.clipboard = "unnamedplus"
+vim.o.cursorcolumn = true
