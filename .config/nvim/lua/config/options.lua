b5376fc15a565cebe25da4cdbddbd7fbb3c55d96
Yu Xiang <yxiang@singulargenomics.com>
2024-08-22 12:45:23 -0700

    change winbar position

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,7 +3,7 @@
 -- Add any additional options here
 local opt = vim.opt
 opt.pumblend = 0
-opt.winbar = "%=%m %f"
+opt.winbar = "  %m %f"
 opt.clipboard = "unnamedplus"
 vim.o.cursorcolumn = true
 opt.cursorline = false
