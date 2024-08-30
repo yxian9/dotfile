7d63b89222bdc4ead6320d500f673c857eead793
YX <yxiang@local.attlocal.net>
2024-08-29 23:11:38 -0700

    fix noice

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("mini.indentscope").setup({
+  symbol = "│",
+  options = { try_as_border = true },
+  draw = {
+    animation = require("mini.indentscope").gen_animation.none(),
+  },
+})
--- .config/nvim/lua/plugins/disable.lua
+++ .config/nvim/lua/plugins/disable.lua
@@ -2,6 +2,8 @@ return {
   -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   {
     "folke/noice.nvim",
+    enabled = true,
+    commit = "d9328ef903168b6f52385a751eb384ae7e906c6f",
     opts = {
       lsp = {
         signature = {
