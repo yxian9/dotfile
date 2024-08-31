0d58286ff55c1aae80dce4c3443199548e6d1aeb
YX <yux.uth@gmail.com>
2024-08-30 18:09:54 -0700

    add fish git promp

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -87,6 +87,29 @@ bind -M insert \cr _fzf_search_history
 
 set -g fish_escape_delay_ms 30
 
+## git promt
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+
+set -g __fish_git_prompt_color_branch magenta
+set -g __fish_git_prompt_showupstream informative
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
+
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_dirtystate "+"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_conflictedstate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
+
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate green
+
+
 ## fzf cd
 function fcd
     # Get a list of directories, ignore errors
