7f1fd20ea967f35f23a96594628459ea1e48fbe9
YX <yux.uth@gmail.com>
2024-09-01 10:29:36 -0700

    update avante dependency

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -7,7 +7,8 @@ return {
     provider = "copilot",
   },
   dependencies = {
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "echasnovski/mini.icons",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
