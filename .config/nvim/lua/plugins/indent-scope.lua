134879175f33a2ed635bb79e6d1c743a6af995cf
YX <yux.uth@gmail.com>
2024-10-12 12:02:18 -0700

    update lualine indent UI stype

--- .config/nvim/lua/plugins/indent-scope.lua
+++ .config/nvim/lua/plugins/indent-scope.lua
@@ -2,10 +2,11 @@ return {
   "mini.indentscope",
   config = function()
     require("mini.indentscope").setup({
-      symbol = "│",
+      symbol = "┋",
       options = { try_as_border = true },
       draw = {
         animation = require("mini.indentscope").gen_animation.none(),
+        delay = 150,
       },
     })
   end,
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -6,11 +6,12 @@ return {
   },
   opts = function(_, opts)
     local icons = LazyVim.config.icons
-    opts.options.theme = "auto"
-    opts.options.component_separators = { left = ">", right = "│" }
+    opts.options.theme = "gruvbox_dark"
+    opts.options.component_separators = { left = ">", right = "⏽" }
     opts.options.section_separators = ""
     opts.sections.lualine_a = {
-      { "fancy_mode", width = 3 },
+      -- { "fancy_mode" }, --, width = 5 },
+      { "mode" },
     }
     opts.sections.lualine_c = {
       LazyVim.lualine.root_dir(),
@@ -29,10 +30,14 @@ return {
         separator = "",
         padding = { left = 1, right = 0 },
       },
+      {
+        "navic",
+        color_correction = "dynamic",
+        padding = { right = 0 },
+      },
     }
     -- https://github.com/LazyVim/LazyVim/pull/4231
-    table.insert(opts.sections.lualine_c, { "navic", color_correction = "dynamic" })
-
+    -- table.insert(opts.sections.lualine_c, { "navic", color_correction = "dynamic" })
     -- table.insert(opts.sections.lualine_c, {
     --   function()
     --     return require("nvim-navic").get_location()
