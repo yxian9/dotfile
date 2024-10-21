74dcd8d6429882c51c4cd5af37713435f83e2b4a
YX <yux.uth@gmail.com>
2024-10-20 21:24:25 -0700

    update leetcode config

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,23 +2,31 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 --
-vim.api.nvim_create_augroup("cursorline_cursorcolumn", { clear = true })
-vim.api.nvim_create_autocmd("InsertLeave", {
-  group = "cursorline_cursorcolumn",
-  pattern = "*",
-  command = "set cursorline",
-})
-vim.api.nvim_create_autocmd("InsertEnter", {
-  group = "cursorline_cursorcolumn",
-  pattern = "*",
-  command = "set nocursorline",
-})
+-- vim.api.nvim_create_augroup("cursorline_cursorcolumn", { clear = true })
+-- vim.api.nvim_create_autocmd("InsertLeave", {
+--   group = "cursorline_cursorcolumn",
+--   pattern = "*",
+--   command = "set cursorline",
+-- })
+-- vim.api.nvim_create_autocmd("InsertEnter", {
+--   group = "cursorline_cursorcolumn",
+--   pattern = "*",
+--   command = "set nocursorline",
+-- })
 --
 vim.api.nvim_create_augroup("auto_save", { clear = true })
 vim.api.nvim_create_autocmd("FocusLost", {
   group = "auto_save",
   pattern = "*",
   command = "wa",
+})
+
+-- set linenumber
+-- https://github.com/LazyVim/LazyVim/discussions/1892
+vim.api.nvim_create_autocmd("BufRead", {
+  callback = function()
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "#ff9900" })
+  end,
 })
 
 local augroup = vim.api.nvim_create_augroup
--- .config/nvim/lua/plugins/leetcode.lua
+++ .config/nvim/lua/plugins/leetcode.lua
@@ -5,20 +5,48 @@ return {
     "nvim-telescope/telescope.nvim",
     "nvim-lua/plenary.nvim", -- required by telescope
     "MunifTanjim/nui.nvim",
-
     -- optional
     "nvim-treesitter/nvim-treesitter",
     "rcarriga/nvim-notify",
     "nvim-tree/nvim-web-devicons",
   },
   lazy = "leetcode.nvim" ~= vim.fn.argv()[1],
+  keys = {
+    { "<leader>9l", "<cmd>Leet list<cr>", { desc = "leetcode list" } },
+    { "<leader>9c", "<cmd>Leet lang<cr>", { desc = "leetcode lang" } },
+    { "<leader>9s", "<cmd>Leet submit<cr>", { desc = "leetcode submit" } },
+    { "<leader>9r", "<cmd>Leet run<cr>", { desc = "leetcode run" } },
+    { "<leadey>9y", "<cmd>Leet yank<cr>", { desc = "leetcode yank" } },
+  },
   opts = {
     arg = "leetcode.nvim",
-    lang = "typescript",
+    lang = "golang",
     cn = { -- leetcode.cn
       enabled = true, ---@type boolean
       translator = false, ---@type boolean
       translate_problems = false, ---@type boolean
+    },
+    injector = {
+      ["python3"] = {
+        before = {
+          "from typing import Optional, List",
+          [[
+          class TreeNode:
+              def __init__(self, val=0, left=None, right=None):
+                  self.val = val
+                  self.left = left
+                  self.right = right 
+          ]],
+        },
+      },
+      ["typescript"] = {
+        before = { [[ import { TreeNode} from "./type" ]] },
+      },
+      ["golang"] = {
+        before = {
+          "package leetcode",
+        },
+      },
     },
   },
 }
