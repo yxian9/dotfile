37eabfad738f8bf8cb5cc1098ed5fe8192079f57
YX <yux.uth@gmail.com>
2024-11-17 11:32:12 -0800

    update yazi remove lf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -186,3 +186,11 @@ function fish_mode_prompt
     end
     set_color normal
 end
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
