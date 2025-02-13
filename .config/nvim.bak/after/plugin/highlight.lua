2d852d97def35a9d94ffdf0aa23ea83cfcd1b43b
yxian9 <yux.uth@gmail.com>
2025-02-13 10:32:22 -0800

    remove previouw nvm bak

--- .config/nvim.bak/after/plugin/highlight.lua
+++
@@ -1,1 +0,0 @@
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ff9900" })
--- .config/nvim.bak/lua/plugins/icons.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "echasnovski/mini.icons",
-  opts = {
-    filetype = {
-      -- go = { glyph = "\u{f2af}", hl = "MiniIconsAzure" },
-      go = { glyph = "\u{f07d3}", hl = "MiniIconsAzure" },
-    },
-  },
-}
