7b57f69eff46e18af6b71af65955f0da5c09d6a0
yxian9 <yux.uth@gmail.com>
2025-02-17 08:27:00 -0800

    fix lsp icon missing issue

---
+++ .config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/flash.nvim",
+  -- https://github.com/folke/flash.nvim
+  opts = {
+    modes = {
+      char = {
+        autohide = false,
+      },
+    },
+    remote_op = {
+      restore = false,
+      motion = nil,
+    },
+  },
+}
--- .config/nvim/lua/plugins/round.lua
+++ .config/nvim/lua/plugins/round.lua
@@ -18,15 +18,15 @@ return {
       },
     },
   },
-  {
-    "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      -- keys go here
-      opts.diagnostics = {
-        float = {
-          border = "rounded",
-        },
-      }
-    end,
-  },
+  -- { -- will diable lsp icon ??
+  --   "neovim/nvim-lspconfig",
+  --   opts = function(_, opts)
+  --     -- keys go here
+  --     opts.diagnostics = {
+  --       float = {
+  --         border = "rounded",
+  --       },
+  --     }
+  --   end,
+  -- },
 }
