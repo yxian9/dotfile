5664b65a2dfe7431fb7a11c4ad3409ac4aa946c9
yxian9 <yux.uth@gmail.com>
2025-07-05 16:13:27 -0700

    add cpu monitor

--- .config/sketchybar/items/apple.sh
+++ .config/sketchybar/items/apple.sh
@@ -13,35 +13,35 @@ apple_logo=(
   padding_left=10
   label.drawing=off
   background.drawing=off
-  click_script="$POPUP_CLICK_SCRIPT"
+  # click_script="$POPUP_CLICK_SCRIPT"
 )
 
-apple_prefs=(
-  icon=$PREFERENCES
-  label="Preferences"
-  click_script="open -a 'System Preferences'; $POPUP_OFF"
-)
+# apple_prefs=(
+#   icon=$PREFERENCES
+#   label="Preferences"
+#   click_script="open -a 'System Preferences'; $POPUP_OFF"
+# )
 
-apple_activity=(
-  icon=$ACTIVITY
-  label="Activity"
-  click_script="open -a 'Activity Monitor'; $POPUP_OFF"
-)
+# apple_activity=(
+#   icon=$ACTIVITY
+#   label="Activity"
+#   click_script="open -a 'Activity Monitor'; $POPUP_OFF"
+# )
 
-apple_lock=(
-  icon=$LOCK
-  label="Lock Screen"
-  click_script="pmset displaysleepnow; $POPUP_OFF"
-)
+# apple_lock=(
+#   icon=$LOCK
+#   label="Lock Screen"
+#   click_script="pmset displaysleepnow; $POPUP_OFF"
+# )
 
 sketchybar --add item apple.logo left \
   --set apple.logo "${apple_logo[@]}" \
-  \
-  --add item apple.prefs popup.apple.logo \
-  --set apple.prefs "${apple_prefs[@]}" \
-  \
-  --add item apple.activity popup.apple.logo \
-  --set apple.activity "${apple_activity[@]}" \
-  \
-  --add item apple.lock popup.apple.logo \
-  --set apple.lock "${apple_lock[@]}"
+  # \
+  # --add item apple.prefs popup.apple.logo \
+  # --set apple.prefs "${apple_prefs[@]}" \
+  # \
+  # --add item apple.activity popup.apple.logo \
+  # --set apple.activity "${apple_activity[@]}" \
+  # \
+  # --add item apple.lock popup.apple.logo \
+  # --set apple.lock "${apple_lock[@]}"
--- .config/sketchybar/items/cpu.sh
+++ .config/sketchybar/items/cpu.sh
@@ -3,4 +3,5 @@
 sketchybar --add item cpu right \
   --set cpu update_freq=15 \
   icon=􀧓 \
-  script="$PLUGIN_DIR/cpu.sh"
+  script="$PLUGIN_DIR/cpu.sh" \
+  click_script="open -a 'Activity Monitor'"
